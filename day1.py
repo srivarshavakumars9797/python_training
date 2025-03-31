933cfd802e9407ac6f519aa53d6ff6d6dfe4325c
sri varshva <srivarshavakumar@gmail.com>
2025-03-31 18:41:21 +0530

    First commit

---
+++ day1.py
@@ -0,0 +1,24 @@
+print("Name : Sri varshava kumar sakthi ganesh")
+print("Roll number : 24IR021")
+a="PSG Institute of Advanced studies"
+print(f"College : {a}")
+b = "24IR021"
+print(f"College : {b}")
+
+print(ord("b"))
+print(chr(98))
+
+c=2
+d=1
+
+print("a,b")
+print(f"{c},{d}")
+p = c+d
+q = c-d
+r = c*d
+s = c/d
+
+print(f"Addition = {p} \nSubraction = {q}")
+print(f"Multiplication = {r} \nDivition = {s}")
+
+# here iam learning python
---
+++ first.py
@@ -0,0 +1,27 @@
+print("Hello Python")
+
+a=10
+b="This is value two"
+
+print(f"{a},{b}")
+print("a, b")
+
+numbers=123456789
+
+print(f"numbers={numbers} thid id one of the number set")
+
+print(ord("a"))
+print(ord("A"))
+print(chr(65))
+
+#this is number reference
+
+f=20
+h=30
+
+g=f+h
+r=f-h
+t=f*h
+q=f/h
+
+print(f"add={g} \n sub={r} \n multi={t} \n divide={q}")
